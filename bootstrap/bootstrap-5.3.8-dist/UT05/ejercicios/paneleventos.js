6c25aa6e98525c2d49db190127ecfa1ec8973787
Adrian Vicente Lopez <adrian.vicente.lopez@ces-vegamedia.es>
2026-01-13 12:34:13 +0100

    ejercicio 1 ut5

--- bootstrap/bootstrap-5.3.8-dist/UT05/ejercicios/paneleventos.js
+++ bootstrap/bootstrap-5.3.8-dist/UT05/ejercicios/paneleventos.js
@@ -1,6 +1,52 @@
-const btnclick=document.getElementById('btnclick');
-const txtclics=document.getElementById('txtclics');
-const btnraton=document.getElementById('zonaraton');
-const textoentrada=document.getElementById('textentrada').value;
-const btnreset=document.getElementById('btnreset');
+const btnClick = document.getElementById('btnClick');
+const txtClics = document.getElementById('txtClics');
+const zonaRaton = document.getElementById('zonaRaton');
+const txtEntrada = document.getElementById('txtEntrada');
+const txtTecla = document.getElementById("txtTecla");
+const btnReset = document.getElementById('btnReset');
 
+// estados
+
+let contador = 0;
+
+// eventos clics
+
+btnClick.addEventListener("click", function () {
+    contador++;
+    txtClics.textContent = "Clics: " + contador;
+});
+
+// evento raton
+
+// funcion reutilizable para eventos del raton
+
+function gestionarRaton(event) {
+    if (event.type === "mouseover") {
+        zonaRaton.textContent = "Raton dentro";
+
+    } else if (event.type === "mouseout") {
+        zonaRaton.textContent = "Raton fuera";
+    }
+}
+
+    // asignacion de eventos del raton
+
+    zonaRaton.addEventListener("mouseover", gestionarRaton);
+    zonaRaton.addEventListener("mouseout", gestionarRaton);
+
+    // evento teclado
+    txtEntrada.addEventListener("keydown", function (event) {
+        txtTecla.textContent = "Ultima tecla: " + event.key;
+    });
+
+    // reiniciar
+
+    btnReset.addEventListener("click", function () {
+        contador = 0;
+        txtClics.textContent = "Clics 0";
+        zonaRaton.textContent = "Raton Fuera";
+
+        txtEntrada.value = "";
+        txtTecla.textContent = "Ultima tecla: (ninguna)";
+
+    });
